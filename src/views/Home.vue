5c2aa18d60cbded3fe8003eaa4fc54cf2606d758
Yahdi Indrawan <yahdiindrawan11@gmail.com>
2021-08-22 07:17:25 +0700

    add header component

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Header />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    Header,
+  },
+};
+</script>
