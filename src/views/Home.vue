1783f7ba71dcf9e9e3588899d38e8d62a342a1e7
Yahdi Indrawan <yahdiindrawan11@gmail.com>
2021-08-22 11:38:12 +0700

    create footer component and completed home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@
     <Hero />
     <Products />
     <Insta />
+    <PartnerLogo />
+    <Footer />
   </div>
 </template>
 
@@ -15,6 +17,8 @@ import Header from "@/components/Header.vue";
 import Hero from "@/components/Hero.vue";
 import Products from "@/components/Products.vue";
 import Insta from "@/components/Insta.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Home",
@@ -23,6 +27,8 @@ export default {
     Hero,
     Products,
     Insta,
+    PartnerLogo,
+    Footer,
   },
 };
 </script>
