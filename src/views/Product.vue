a24f1073d06c8425cdee1a6292e541a1189d00d7
Yahdi Indrawan <yahdiindrawan11@gmail.com>
2021-08-22 15:40:52 +0700

    change image from carousel

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,7 +2,7 @@
   <div class="product">
     <Header />
     <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
+    <div class="breacrumb-section text-left">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
@@ -24,36 +24,50 @@
             <div class="row">
               <div class="col-lg-6">
                 <div class="product-pic-zoom">
-                  <img class="product-big-img" src="img/mickey1.jpg" alt="" />
+                  <img class="product-big-img" :src="gambar_default" alt="" />
                 </div>
                 <div class="product-thumbs">
                   <carousel
                     class="product-thumbs-track ps-slider"
                     :dots="false"
                     :nav="false"
-                    :autoplay="true"
-                    :loop="true"
                   >
-                    <div class="pt active" data-imgbigurl="img/mickey1.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[0])"
+                      :class="thumbs[0] === gambar_default ? 'active' : ''"
+                    >
                       <img src="img/mickey1.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey2.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[1])"
+                      :class="thumbs[1] === gambar_default ? 'active' : ''"
+                    >
                       <img src="img/mickey2.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey3.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[2])"
+                      :class="thumbs[2] === gambar_default ? 'active' : ''"
+                    >
                       <img src="img/mickey3.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey4.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[3])"
+                      :class="thumbs[3] === gambar_default ? 'active' : ''"
+                    >
                       <img src="img/mickey4.jpg" alt="" />
                     </div>
                   </carousel>
                 </div>
               </div>
               <div class="col-lg-6">
-                <div class="product-details">
+                <div class="product-details text-left">
                   <div class="pd-title">
                     <span>oranges</span>
                     <h3>Pure Pineapple</h3>
@@ -115,10 +129,26 @@ export default {
     carousel,
     Footer,
   },
+  data() {
+    return {
+      gambar_default: "img/mickey1.jpg",
+      thumbs: [
+        "img/mickey1.jpg",
+        "img/mickey2.jpg",
+        "img/mickey3.jpg",
+        "img/mickey4.jpg",
+      ],
+    };
+  },
+  methods: {
+    changeImage(urlImage) {
+      this.gambar_default = urlImage;
+    },
+  },
 };
 </script>
 <style scoped>
-.product-thumbs .pt {
+/* .product-thumbs .pt {
   margin-right: 15px;
-}
+} */
 </style>
